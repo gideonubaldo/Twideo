1041bedbbc0b29144ca440874a76cecad729956e
gideon ubaldo <gideon.ubaldo@sjsu.edu>
2019-03-04 13:24:21 -0800

    initializing proj

---
+++ Twideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twideo
+//
+//  Created by Gideon Ubaldo on 3/4/19.
+//  Copyright © 2019 Gideon Ubaldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
