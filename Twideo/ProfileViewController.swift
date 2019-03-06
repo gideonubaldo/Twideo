1bb326a0490d723a791dae1892faa5b9b7115c2e
gideon ubaldo <gideon.ubaldo@sjsu.edu>
2019-03-05 16:31:53 -0800

    add profile view

---
+++ Twideo/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  Twideo
+//
+//  Created by Gideon Ubaldo on 3/5/19.
+//  Copyright © 2019 Gideon Ubaldo. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var albumCountTitle: UILabel!
+    @IBOutlet weak var videoCountTitle: UILabel!
+    @IBOutlet weak var shareCountTitle: UILabel!
+    
+    @IBOutlet weak var albumCountLabel: UILabel!
+    @IBOutlet weak var videoCountLabel: UILabel!
+    @IBOutlet weak var shareCountLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        albumCountTitle.numberOfLines = 0
+        [albumCountTitle .sizeToFit()]
+    }
+    
+
+
+}
